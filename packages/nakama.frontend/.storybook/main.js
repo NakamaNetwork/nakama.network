abe7709195890b9b773d526308a79b3cd48f1c4f
Bobby Cafazzo <robocafaz@gmail.com>
2020-10-03 01:55:01 -0400

    Add storybook support

---
+++ packages/nakama.frontend/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../src/**/__stories__/*.story.@(ts|tsx)'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-knobs']
+};
---
+++ packages/nakama.frontend/src/components/__stories__/application.story.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Application } from '../application';
+
+export default {
+  title: 'Application',
+  component: Application
+};
+
+export const DefaultApplication = () => <Application />;
