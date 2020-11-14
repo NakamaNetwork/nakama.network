e036bfb78fb52a714ed6e15cfa37708545becb79
Bobby Cafazzo <robocafaz@gmail.com>
2020-11-13 23:07:48 -0500

    Add infrastructure project
    Can be used to deploy stacks to AWS CloudFormation.

---
+++ packages/nakama.infrastructure/src/stack/__tests__/nakama-infrastructure-stack.test.ts
@@ -0,0 +1,18 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as NakamaInfrastructure from '../nakama-infrastructure-stack';
+
+test('Empty Stack', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new NakamaInfrastructure.NakamaInfrastructureStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {}
+      },
+      MatchStyle.EXACT
+    )
+  );
+});
