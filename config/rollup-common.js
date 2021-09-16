fce8fc7b07dd8b7c787e15161a13ce0ac2c6e162
robocafaz <robocafaz@gmail.com>
2021-09-16 01:15:40 -0400

    Update rollup configs

--- config/rollup-common.js
+++ config/rollup-common.js
@@ -1,8 +1,12 @@
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.ts', '.js'];
+const trigger = process.env.CODEBUILD_SOURCE_VERSION;
+const hash = process.env.CODEBUILD_RESOLVED_SOURCE_VERSION;
+const version = trigger && hash && trigger === hash ? 'prod' : 'test';
 
 export const lambaConfig = (entry, { plugins = [] } = {}) => ({
   input: entry,
@@ -13,9 +17,10 @@ export const lambaConfig = (entry, { plugins = [] } = {}) => ({
     sourcemap: true
   },
   plugins: [
-    nodeResolve({ extensions, preferBuiltins: true }),
+    nodeResolve({ extensions, preferBuiltins: true, browser: false }),
     babel({ babelHelpers: 'bundled', extensions }),
-    terser(),
+    commonjs(),
+    ...(version === 'prod' ? [terser()] : []),
     ...plugins
   ]
 });
